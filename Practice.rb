63a43dbff789ec785b94d73fb4ae8c48469b2a78
JULIA ZHOU <jzhouerm@hotmail.com>
2020-06-11 18:14:48 +0000

    Automatically backed up by Learn

--- Practice.rb
+++ Practice.rb
@@ -1,7 +1,13 @@
 magic_exit_number = 7
 count = 0 
-while count < 10 do
-  break if count == magic_exit_number
-  puts "I am the #{count}, I love to count!"
-    count = count + 1 
-end
+
+#while count < 10 do
+ # break if count == magic_exit_number
+  #puts "I am the #{count}, I love to count!"
+   # count = count + 1 
+#end
+
+while count < 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count efficiently!"
+  count = count + 1 
+end     
